0c7ec6e15e48ef450baaf4b71bb24868ae2c44dc
lminta <shakurov.ve@gmail.com>
2023-01-02 23:58:26 +0400

    Fix rename

--- Assets/SerializableDictionary/Editor/SerializableDictionaryPropertyDrawers.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NotAgain.UI.UIManager;
-using UnityEngine;
-using UnityEditor;
-
-[CustomPropertyDrawer(typeof(UITypeUIWindowDictionary))]
-public class AnySerializableDictionaryPropertyDrawer : SerializableDictionaryPropertyDrawer {}
-
---
+++ Assets/ThirdParty/SerializableDictionary/Editor/SerializableDictionaryPropertyDrawers.cs
@@ -0,0 +1,8 @@
+﻿using NotAgain.Core.Scenes;
+using NotAgain.UI.UIManager;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(UIWindowIDUIWindowDictionary))]
+[CustomPropertyDrawer(typeof(SceneIDNameDictionary))]
+public class AnySerializableDictionaryPropertyDrawer : SerializableDictionaryPropertyDrawer {}
+
